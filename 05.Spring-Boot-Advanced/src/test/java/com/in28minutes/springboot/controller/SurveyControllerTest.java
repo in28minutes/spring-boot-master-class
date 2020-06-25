362dde4914be0dfdf222eb5ec360808ff29a3bc4
Ranga Karanam <rangakaranam@Rangas-MacBook-Air.local>
2020-06-25 11:39:36 +0530

    Thank You for Choosing to Learn from in28Minutes

--- 05.Spring-Boot-Advanced/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
+++ 05.Spring-Boot-Advanced/src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -25,7 +26,8 @@ import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.service.SurveyService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(value = SurveyController.class, secure = false)
+@WebMvcTest(value = SurveyController.class)
+@WithMockUser
 public class SurveyControllerTest {
 
 	@Autowired
