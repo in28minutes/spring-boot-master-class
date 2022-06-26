96e8461588f6dc0b636f49be4fe9d39c051a509a
Ranga Karanam <in28minutes@gmail.com>
2022-06-26 19:58:16 +0530

    Thank You for Choosing to Learn from in28Minutes

--- 02.Spring-Boot-Web-Application-V2/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ 02.Spring-Boot-Web-Application-V2/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
